34d3c5981f439690c262bebc441c9084d9b79e7e
user <user@175.117.247.7>
2021-10-24 14:30:30 +0900

    지금까지 나온 로또번호 DB에 옮겨담기

--- Lotto/src/Lotto/Lotto.java
+++ Lotto/src/Lotto/Lotto.java
@@ -1,21 +1,29 @@
 package Lotto;
 
+import java.util.HashMap;
+
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import db.Script;
+
 public class Lotto {
 	private WebDriver driver;
 	public static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
+//	public static final String WEB_DRIVER_PATH = "C:\\chromedriver.exe";
 	public static final String WEB_DRIVER_PATH = "E:\\chromedriver.exe";
 
 	public static void main(String[] args) {
+		Script script = new Script();
 		Lotto lotto = new Lotto();
-		int count = 985;
+		int count = 986;
+	
 		
-		String [][] number = new String[count][7];
+		
 
 		System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
 		ChromeOptions options = new ChromeOptions();
@@ -25,28 +33,44 @@ public class Lotto {
 		String url = "https://www.dhlottery.co.kr/common.do?method=main";
 		lotto.driver.get(url);
 		
+		
+		
 		lotto.driver.findElement(By.id("lottoDrwNo"));
 		
-		
-		for (int i = 0; i < 985; i++) {
+		for (int i = 0; i < 986; i++) {
+			int[] number = new int[8];
+			int bonus = 1;
 			System.out.print(count + " 회차 : ");
 			for (WebElement el : lotto.driver.findElements(By.className("ball_645"))) {
-				System.out.print(el.getText() + " ");
+				if (bonus == 7) {
+					int num = Integer.parseInt(el.getText());
+					number[bonus] = num;
+					System.out.print("+ " + el.getText());
+				} else {
+					int num = Integer.parseInt(el.getText());
+					number[bonus] = num;
+					System.out.print(el.getText() + " ");
+				}
+				bonus++;
 			}
-			count--;
 			System.out.println();
-			
-			lotto.driver.findElement(By.className("prev")).click();
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				;
-			}	
+			if (i != 985) {
+				lotto.driver.findElement(By.className("prev")).click();
+				try {
+					Thread.sleep(30);
+				} catch (InterruptedException e) {
+					;
+				}
+			}
+			script.put(count,number);
+			count--;
 		}
 		
 
-
 		lotto.driver.close();
 		lotto.driver.quit();
+		
+		
 	}
+
 }
